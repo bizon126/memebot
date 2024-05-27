8d58e8a21f95fff0aeb914ab364c9d352a6f10eb
bizon126 <oleg.agapitov@gmail.com>
2024-05-27 15:10:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -38,10 +38,11 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 var grammy_1 = require("grammy");
-require("dotenv/config");
 var node_fetch_1 = require("node-fetch");
 var jsdom_1 = require("jsdom");
-var BOT_TOKEN = ((_a = process === null || process === void 0 ? void 0 : process.env) === null || _a === void 0 ? void 0 : _a.BOT_TOKEN) || '7063922552:AAHML0RzcpoQCOaZzPHbopIgQ6WxVLDB620';
+var dotenv = require("dotenv");
+dotenv.config({ path: __dirname + '/.env' });
+var BOT_TOKEN = (_a = process === null || process === void 0 ? void 0 : process.env) === null || _a === void 0 ? void 0 : _a.BOT_TOKEN;
 var DEST = 'https://www.memify.ru/highfive/';
 var bot = new grammy_1.Bot(BOT_TOKEN);
 function getMemes() {
